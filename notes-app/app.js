2f14d14e4c6e7c168fea99fb3b000e4071c9f30b
tulsani, Bharat (CW) <bharat.tulsani@hp.com>
2024-12-12 20:26:45 +0530

    Initiall commit

---
+++ notes-app/app.js
@@ -0,0 +1,6 @@
+// const fs=require('fs')
+// fs.writeFileSync('notes.txt','This file was created by Node.js!')
+// fs.appendFileSync('notes.txt','This is the appended text.')
+
+require('./utils.js')
+console.log('app.js is running')
