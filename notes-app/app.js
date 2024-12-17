38aab5d25e89f0ad649ea0691be740867df747da
tulsani, Bharat (CW) <bharat.tulsani@hp.com>
2024-12-17 19:18:08 +0530

    Initiall commit

--- notes-app/app.js
+++ notes-app/app.js
@@ -11,6 +11,17 @@
 // console.log(validator.isEmail('bharattulsani21@gmail.com'))
 // console.log(validator.isURL('bharattulsani21@gmail.com'))
 
-const fs=require('fs')
-const contents=fs.readFileSync('notes.txt','utf8')
-console.log(contents)
+// const fs=require('fs')
+// const contents=fs.readFileSync('notes.txt','utf8')
+// console.log(contents)
+// const chalk=require('chalk')
+// console.log(chalk.blue('Hello World!'))
+// console.log(chalk.green('Success!'))
+// console.log(process.argv[2])
+console.log(process.argv)
+const command=process.argv[2]
+if (command==='add'){
+    console.log('Adding note!')
+}else if(command==='remove'){
+    console.log('Removing note!')
+}
