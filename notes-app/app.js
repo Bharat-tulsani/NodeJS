8d3d03318a5e66e53fd83749ba422de00412e831
tulsani, Bharat (CW) <bharat.tulsani@hp.com>
2024-12-17 19:38:50 +0530

    Initiall commit

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,6 @@
+const chalk=require('chalk')
+const yargs=require('yargs')
+
 // const fs=require('fs')
 // fs.writeFileSync('notes.txt','This file was created by Node.js!')
 // fs.appendFileSync('notes.txt','This is the appended text.')
@@ -14,14 +17,42 @@
 // const fs=require('fs')
 // const contents=fs.readFileSync('notes.txt','utf8')
 // console.log(contents)
-// const chalk=require('chalk')
 // console.log(chalk.blue('Hello World!'))
 // console.log(chalk.green('Success!'))
 // console.log(process.argv[2])
-console.log(process.argv)
-const command=process.argv[2]
-if (command==='add'){
-    console.log('Adding note!')
-}else if(command==='remove'){
-    console.log('Removing note!')
-}
+// console.log(process.argv)
+
+yargs.command({
+    command:'remove',
+    describe:'Removing a note',
+    handler:function(){
+        console.log('Removing a note!')
+    }
+})
+// Define the 'add' command with different cases
+yargs.command({
+    command: ['add', 'Add', 'ADD'],
+    describe: 'Add a new note',
+    handler: function () {
+        console.log('Adding a new note!');
+    }
+});
+
+// Define the 'list' command with different cases
+yargs.command({
+    command: ['list', 'List', 'LIST'],
+    describe: 'Listing notes',
+    handler: function () {
+        console.log('Listing notes!');
+    }
+});
+
+// Define the 'read' command with different cases
+yargs.command({
+    command: ['read', 'Read', 'READ'],
+    describe: 'Reading a note',
+    handler: function () {
+        console.log('Reading a note!');
+    }
+});
+console.log(yargs.argv)
